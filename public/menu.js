6b5537e84b312cd3b204c6bf35e5643ee8c51a82
nima-fh <151225313+nima-fh@users.noreply.github.com>
2024-04-16 22:10:14 +0330

    final project with tailwindcss

---
+++ public/menu.js
@@ -0,0 +1,9 @@
+const burger = document.querySelector("#bars-icon");
+const menu = document.querySelector("#bars");
+burger.addEventListener("click", () => {
+  if (menu.classList.contains("hidden")) {
+    menu.classList.remove("hidden");
+  } else {
+    menu.classList.add("hidden");
+  }
+});
